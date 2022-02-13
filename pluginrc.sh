1d9bf76c4ac64675a0af433a5582dade35a3f7d1
Mike Crowe <drmikecrowe@gmail.com>
2022-02-13 11:31:08 -0500

    removing -x

--- pluginrc.sh
+++ pluginrc.sh
@@ -1,7 +1,5 @@
-set -x
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 plugin_name="$(basename $(dirname $(dirname ${BASH_SOURCE[0]})))"
 BASH_IT="$CURR_DIR/.bash_it"
-set +x
 
 source $BASH_IT/personal-startup.sh
